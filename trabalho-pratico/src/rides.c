7a979372e41bbbc207dfa93d1dcee152d5fe7ff7
a100665 <a100665@alunos.uminho.pt>
2022-11-15 19:19:51 +0000

    Fix nos catálogos e atualizei alguns tipos

--- trabalho-pratico/src/rides.c
+++ trabalho-pratico/src/rides.c
@@ -8,24 +8,28 @@
 
 #include <glib.h>
 
+#include "query1.c"
+#include "query2.c"
+
 struct rides {
   char * id;
   char * date;
   char * driver;
   char * user;
   char * city;
-  char * distance;
-  char * score_user;
-  char * score_driver;
-  char * tip;
+  short int distance;
+  short int score_user;
+  short int score_driver;
+  double tip;
   char * comment;
 };
 
+/*
 char* rides_catalog(char* id) {
   char * line = NULL;
   ssize_t read;
   size_t len;
-  GHashTable * hash = g_hash_table_new(g_str_hash, g_str_equal);
+  GHashTable * hash_rides = g_hash_table_new(g_str_hash, g_str_equal);
   FILE * file = fopen("rides.csv", "r");
   if (file == NULL) {
     printf("Error opening file.\n");
@@ -41,7 +45,6 @@ char* rides_catalog(char* id) {
         switch (i) {
         case 0:
           r -> id = strdup(token);
-          
           break;
         case 1:
           r -> date = strdup(token);
@@ -74,7 +77,7 @@ char* rides_catalog(char* id) {
       i++;
       }
       //char *line_copy = strdup(line);
-      g_hash_table_insert(hash, r -> id, r);
+      g_hash_table_insert(hash_rides, r -> id, r);
     }
     if (read == 7) printf("%d",i);
     if (read != 7 && !feof(file)) {
@@ -82,12 +85,12 @@ char* rides_catalog(char* id) {
     }
     i++;
   } while (!feof(file));
-  printf("Tamanho da hash table: %d\n", g_hash_table_size(hash));
-  struct rides * user_pretendido = g_hash_table_lookup(hash, id);
+  printf("Tamanho da hash table: %d\n", g_hash_table_size(hash_rides));
+  struct rides * user_pretendido = g_hash_table_lookup(hash_rides, id);
   printf("%s", user_pretendido->distance);
 
   fclose(file);
-  g_hash_table_destroy(hash);
+ // g_hash_table_destroy(hash);
 
   printf("\n%d records read.\n\n", records);
   FILE * output = fopen("output.txt", "w+");
@@ -101,16 +104,109 @@ char* rides_catalog(char* id) {
     "%s;"
     "%s", user_pretendido-> id, user_pretendido-> date, user_pretendido-> driver, user_pretendido-> user, user_pretendido-> score_user, user_pretendido-> distance, user_pretendido-> score_user, user_pretendido-> score_driver, user_pretendido->tip);
 
-char* v = user_pretendido->driver;
-  free  (user_pretendido);
+char* v = hash_rides;
 return v;
 }
 
-int main() {
-  rides_catalog("1000000");
-    printf( "ola");
-  //struct drivers** p = catalog;
-  //printf("%p",p);
-  return 0;
-}
+*/
 
+void rides_catalog(GHashTable * users_hash, GHashTable * drivers_hash) {
+  char * line = NULL;
+  ssize_t read;
+  size_t len;
+ // GHashTable * hash_rides = g_hash_table_new(g_str_hash, g_str_equal);
+  FILE * file = fopen("rides.csv", "r");
+  if (file == NULL) {
+    printf("Error opening file.\n");
+  }
+  //int records = 0;
+  int i = 0;
+        int j= 0;
+
+  do {
+    while ((read = getline( & line, & len, file)) != -1) {
+      struct rides * ride = malloc(sizeof(struct rides));
+      char * token;
+      int i = 0;
+      while ((token = strsep( & line, ";\n"))) {
+        switch (i) {
+        case 0:
+          ride -> id = strdup(token);
+          break;
+        case 1:
+          ride -> date = strdup(token);
+          break;  
+        case 2:
+          ride -> driver = strdup(token);
+          break;
+        case 3:
+          ride -> user = strdup(token);
+          break;
+        case 4:
+          ride -> city = strdup(token);
+          break;
+        case 5:
+          ride -> distance = atof(token);
+          break;
+        case 6:
+          ride -> score_user = atof(token);
+          break;
+        case 7:
+          ride -> score_driver = atof(token);
+          break;
+        case 8:
+          ride -> tip = atof(token);
+          break;
+        case 9:
+          ride -> comment = strdup(token);
+          break;
+        }
+      i++;
+
+      }      //escrever aqui o que colocar a cada iteracao de user
+      if (j== 0) {;} else {
+
+      // estou a fazer malloc de algo que já tinha sido previamente alocado na mem
+         struct users * u = malloc(sizeof(struct users));
+         struct drivers * d = malloc(sizeof(struct drivers));
+         u = g_hash_table_lookup (users_hash,ride->user);
+         d = g_hash_table_lookup (drivers_hash,ride->driver);
+        
+        u->total_gasto += calcula_total_gasto(d->car_class,ride->distance,ride->tip);
+        u->avaliacao_total_user += ride->score_user;
+        u->numero_viagens_user++;
+
+        d->total_auferido += calcula_total_gasto (d->car_class,ride->distance,ride->tip);
+        d->avaliacao_total_driver += ride->score_driver;
+        d->numero_viagens_driver++;
+        //printf ("%d\n",u->total_gasto);
+      }     j++;
+
+      //char *line_copy = strdup(line);
+      //g_hash_table_insert(hash_rides, ride -> id, ride);
+    }
+    i++;
+  } while (!feof(file));
+
+ // printf("Tamanho da hash table: %d\n", g_hash_table_size(hash_rides));
+ // struct rides * user_pretendido = g_hash_table_lookup(hash_rides, id);
+ // printf("%s", user_pretendido->distance);
+
+  fclose(file);
+ // g_hash_table_destroy(hash);
+
+  //printf("\n%d records read.\n\n", records);
+  //FILE * output = fopen("output.txt", "w+");
+  //fprintf(output, "%s;"
+   // "%s;"
+   //"%s;"
+   // "%s;"
+   // "%s;"
+   // "%s;"
+   // "%s;"
+   // "%s;"
+   // "%s", user_pretendido-> id, user_pretendido-> date, user_pretendido-> driver, user_pretendido-> user, user_pretendido-> score_user, user_pretendido-> distance, user_pretendido-> score_user, user_pretendido-> score_driver, user_pretendido->tip);
+
+//char* v = hash_rides;
+//return v;
+}
