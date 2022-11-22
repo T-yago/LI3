44ab06562c118df0d5a0937d18821b6d9159a4e3
Ze-Limao <jeswitch.24@gmail.com>
2022-11-22 19:29:14 +0000

    fiz o encapsulamento da Q2 e Q3

--- trabalho-pratico/includes/drivers.h
+++ trabalho-pratico/includes/drivers.h
@@ -7,12 +7,16 @@ typedef struct drivers Drivers;
 
 GHashTable * drivers_catalog (char* pathfiles);
 void initHash_drivers (GHashTable * hash_drivers);
-char * getCarClassDriver(GHashTable * drivers_hash, Drivers * copyDrivers);
 void totalAuferidoDriver(GHashTable * drivers_hash, Drivers * copyDrivers, double ta);
 void avaliacaoTotalDriver(GHashTable * drivers_hash, Drivers * copyDrivers, short int r);
 void numeroViagensDriver(GHashTable * drivers_hash, Drivers * copyDrivers);
 void dateDriver(GHashTable * drivers_hash, Drivers * copyDrivers, unsigned short int r);
-
+char * getIdDriver(GHashTable * drivers_hash, Drivers * copyDrivers);
+bool   getAccountStatus(GHashTable * drivers_hash, Drivers * copyDrivers);
+double getAvaliacaoMediaDriver(GHashTable * drivers_hash, Drivers * copyDrivers);
+double getDateDriver(GHashTable * drivers_hash, Drivers * copyDrivers);
+char * getNameaDriver(GHashTable * drivers_hash, Drivers * copyDrivers);
+char * getCarClassDriver(GHashTable * drivers_hash, Drivers * copyDrivers);
 
 
 #endif
