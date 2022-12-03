fe13e9238323080520a7d4cd27558bf2223cd7ce
Zé <101342495+Ze-Limao@users.noreply.github.com>
2022-12-03 16:53:46 +0000

    inicializacao Q5 (#11)

--- trabalho-pratico/includes/drivers.h
+++ trabalho-pratico/includes/drivers.h
@@ -6,27 +6,28 @@
 typedef struct drivers Drivers;
 typedef struct catalog_drivers Catalog_Drivers;
 
-Catalog_Drivers * drivers_catalog(char * pathfiles);
-void initHash_drivers(Catalog_Drivers * catalog_drivers);
-void free_hash_drivers (Catalog_Drivers * catalog_drivers);
-void totalAuferidoDriver(Catalog_Drivers * catalog_drivers, char * key, double ta);
-void avaliacaoTotalDriver(Catalog_Drivers * catalog_drivers, char * key, short int r);
-void numeroViagensDriver(Catalog_Drivers * catalog_drivers, char * key);
-void dateDriver(Catalog_Drivers * catalog_drivers, char * key, unsigned short int r);
-char * getIdDriver(Catalog_Drivers * catalog_drivers, char * key);
-bool   getAccountStatus(Catalog_Drivers * catalog_drivers, char * key);
-double getAvaliacaoMediaDriver(Catalog_Drivers * catalog_drivers, char * key);
-double getDateDriver(Catalog_Drivers * catalog_drivers, char * key);
-char * getNameDriver(Catalog_Drivers * catalog_drivers, char * key);
-char   getGenderDriver(Catalog_Drivers * catalog_drivers, char * key);
-char* getBirthDayDriver(Catalog_Drivers * catalog_drivers, char * key);
-double getTotalAuferidoDriver(Catalog_Drivers * catalog_drivers, char * key);
-char * getCarClassDriver(Catalog_Drivers * catalog_drivers, char * key);
-int getAvaliacaoTotalDriver(Catalog_Drivers * catalog_drivers, char * key);
-int getNviagensDriver(Catalog_Drivers * catalog_drivers, char * key);
-void avaliacaoMediaDriver(Catalog_Drivers * catalog_drivers, char * key, double r);
-uint get_hash_drivers_size (Catalog_Drivers * catalog_drivers);
-gpointer * get_hash_keys_as_array_drivers (Catalog_Drivers * catalog_drivers, uint size);
+
+GHashTable * drivers_catalog (char* pathfiles);
+void initHash_drivers (GHashTable * hash_drivers);
+void totalAuferidoDriver(GHashTable * drivers_hash, char * key, double ta);
+void totalAuferidoDriverNoTips(GHashTable * drivers_hash, char * key, double ta0);
+void avaliacaoTotalDriver(GHashTable * drivers_hash, char * key, short int r);
+void numeroViagensDriver(GHashTable * drivers_hash, char * key);
+void dateDriver(GHashTable * drivers_hash, char * key, unsigned short int r);
+char * getIdDriver(GHashTable * drivers_hash, char * key);
+bool   getAccountStatus(GHashTable * drivers_hash, char * key);
+double getAvaliacaoMediaDriver(GHashTable * drivers_hash, char * key);
+double getDateDriver(GHashTable * drivers_hash, char * key);
+char * getNameaDriver(GHashTable * drivers_hash, char * key);
+char   getGenderDriver(GHashTable * drivers_hash, char * key);
+char * getBirthDayDriver(GHashTable * drivers_hash, char * key);
+double getTotalAuferidoDriver(GHashTable * drivers_hash, char * key);
+char * getCarClassDriver(GHashTable * drivers_hash, char * key);
+int getAvaliacaoTotalDriver(GHashTable * drivers_hash, char * key);
+int getNviagensDriver(GHashTable * drivers_hash, char * key);
+void avaliacaoMediaDriver(GHashTable * drivers_hash, char * key, double r);
+
+
 
 
 #endif
