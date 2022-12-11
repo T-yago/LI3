0efe3823359d54cf9be5c12bc82d5380f82b2921
a100665 <101202571+T-yago@users.noreply.github.com>
2022-12-11 16:20:23 +0000

    Fix memory leaks query4

--- trabalho-pratico/includes/query4.h
+++ trabalho-pratico/includes/query4.h
@@ -13,6 +13,7 @@ Catalog_Cities *  init_hash_cities ();
 void hash_cities_get_n_viagens (Catalog_Cities * catalog_cities, char * city);
 void insert_cities_hash (Catalog_Cities * cities_catalog , char * city, double total_gasto_por_ride);
 void query4 (Catalog_Cities * catalog_cities, char * city , int n);
+void free_hash_cities (Catalog_Cities * catalog_cities);
 
 
 #endif
--- trabalho-pratico/src/query4.c
+++ trabalho-pratico/src/query4.c
@@ -14,13 +14,14 @@
     };
 
     struct cities {
+    char * key;
     double total_gasto;
     int num_rides;
     };
 
 
     Catalog_Cities * init_hash_cities () {
-        GHashTable * cities_hash = g_hash_table_new (g_str_hash, g_str_equal);
+        GHashTable * cities_hash = g_hash_table_new_full (g_str_hash, g_str_equal,g_free,g_free);
         Catalog_Cities * catalog_cities = malloc  (sizeof (struct catalog_cities));
         catalog_cities->cities_hash  = cities_hash;
         catalog_cities->cities_list = malloc (159 * sizeof (char*)); //TROCAR ISTO DEPOIS
@@ -32,62 +33,73 @@
 
 
     int check_city (Catalog_Cities * cities_catalog, char * city) {
-        int found = 0;
-                  //  printf ("Dentro da check_city: %p\n",cities_catalog->cities_hash);
-        
+        int found = 0;        
         int size = 0;
+           
             for (int i =0; cities_catalog->cities_list[i]; i++) { 
-    size++;
+                size++;
             }
-        for (int i =0; cities_catalog->cities_list[i]; i++) { 
+            for (int i =0; cities_catalog->cities_list[i]; i++) 
+            { 
         
-
-            if (!strcmp (city, cities_catalog->cities_list[i])) {
+                if (!strcmp (city, cities_catalog->cities_list[i])) {
                 found = 1;
-            }
+                }
 
-        }
-        if (found == 0) {
-                cities_catalog->cities_list[size] = strdup (city);
             }
-        //printf ("%s não estava na lista\n", city);
+        if (found == 0) 
+        {
+                cities_catalog->cities_list[size] = strdup (city);
+        }
         if (cities_catalog->cities_list[0] == NULL) cities_catalog->cities_list[0] = strdup (city);
-        //printf ("acabou o check_city\n\n");
         return found;
     }
 
 
-    void insert_cities_hash (Catalog_Cities * cities_catalog , char * city, double total_gasto_por_ride) {
-            char* aux = strdup (city);
-            Cities * cities = malloc (sizeof (struct cities));
-                if (check_city (cities_catalog,city) == 0) {
-                    cities->total_gasto = 0;
-                    cities->total_gasto += total_gasto_por_ride;
-                    cities->num_rides =0;
-                    cities->num_rides +=1;
+void insert_cities_hash (Catalog_Cities * cities_catalog , char * city, double total_gasto_por_ride) {
+            if (check_city (cities_catalog,city) == 0) {
+                char* key = strdup (city);
+                Cities * cities = malloc (sizeof (struct cities));
+                cities->total_gasto = 0;
+                cities->key = key;
+                cities->total_gasto += total_gasto_por_ride;
+                cities->num_rides =0;
+                cities->num_rides +=1;
 
-                    g_hash_table_insert (cities_catalog->cities_hash, aux, cities); 
-                 //   printf ("Num_rides: %d\n",cities->num_rides);
-                }
-        // Cities * cities = malloc (sizeof (struct cities));  
-        else {
-                    Cities * cities = g_hash_table_lookup (cities_catalog->cities_hash,aux);
-                    cities->num_rides++;
-                    cities->total_gasto += total_gasto_por_ride;
-                //    printf ("Num_rides: %d",cities->num_rides);
-          //  printf ("vai checkar na hash\n\n");
-        }              //   printf ("Num_rides: %f\n",aux2->total_gasto);
+                g_hash_table_insert (cities_catalog->cities_hash, key, cities); 
 
+            }
+    else {
+                Cities * cities = g_hash_table_lookup (cities_catalog->cities_hash,city);
+                cities->num_rides++;
+                cities->total_gasto += total_gasto_por_ride;
+    }     
+}
 
+void query4 (Catalog_Cities * catalog_cities, char * info , int n) {
+    char* city = strdup(info);
+    Cities * aux = g_hash_table_lookup (catalog_cities->cities_hash, city);
+    double avaliacao_media = aux->total_gasto / aux->num_rides;
+
+    char buffer[256];
+    snprintf(buffer, 256, "Resultados/command%d_output.txt", n);
+    FILE * output = fopen(buffer, "w");
+    fprintf(output, "%.3f\n",avaliacao_media);
+    free (city);
+}
+
+void free_hash_cities (Catalog_Cities * catalog_cities) {
+    uint size = g_hash_table_size ( catalog_cities->cities_hash);
+    Cities *city ;
+    gpointer * keys = g_hash_table_get_keys_as_array (catalog_cities->cities_hash, &size);
+    //gpointer * keys = g_hash_table_get_keys_as_array ( hash_drivers, &size);
+    for (uint i = 0; i < size; i++) {
+    city = g_hash_table_lookup(catalog_cities->cities_hash, keys[i]);
+    free (city->key);
+    free (city);
     }
-
-    void query4 (Catalog_Cities * catalog_cities, char * info , int n) {
-          char* city = strdup(info);
-        Cities * aux = g_hash_table_lookup (catalog_cities->cities_hash, city);
-        double avaliacao_media = aux->total_gasto / aux->num_rides;
-
-        char buffer[256];
-        snprintf(buffer, 256, "Resultados/command%d_output.txt", n);
-        FILE * output = fopen(buffer, "w");
-        fprintf(output, "%.3f\n",avaliacao_media);
+    free (keys);
+    g_hash_table_destroy (catalog_cities->cities_hash);
+    for (int i=0;i < 159;i++) free (catalog_cities->cities_list[i]);
+    free (catalog_cities->cities_list);
     }
