3dd03467b5607c9bc14e16128764fb063255e7f6
a100665 <101202571+T-yago@users.noreply.github.com>
2022-12-20 20:51:30 +0000

    Fix query6

--- trabalho-pratico/src/query6.c
+++ trabalho-pratico/src/query6.c
@@ -16,6 +16,7 @@
 
 
 void query6 (char * city, char * dateA, char * dateB, Catalog_Rides * catalog_rides, int n) {
+    char* city_query = strdup (city);
     unsigned short int date = 0; 
     double distancia_media = 0;
     uint num_rides = 0;
@@ -29,7 +30,7 @@ void query6 (char * city, char * dateA, char * dateB, Catalog_Rides * catalog_ri
         char * ride_city = (get_ride_city (catalog_rides,keys[i]));
         date = get_ride_date (catalog_rides,keys[i]);
         int distance = get_ride_distance(catalog_rides,keys[i]);
-        if ((date > dateinf) && (date <= datesup) && (!strcmp(strdup(city),ride_city))){
+        if ((!strcmp(city_query,ride_city) && (date > dateinf) && (date <= datesup))){
             distancia_media += distance;
             num_rides++;
         }
@@ -38,11 +39,12 @@ void query6 (char * city, char * dateA, char * dateB, Catalog_Rides * catalog_ri
     }
     if (num_rides != 0)    distancia_media = distancia_media / num_rides;
     else    distancia_media = 0;
-    free (keys);
     char buffer[256];
     snprintf(buffer, 256, "Resultados/command%d_output.txt", n);
 
     FILE * output = fopen(buffer, "w");
     fprintf(output, "%.3f\n", distancia_media);
     fclose(output);
+    free (keys);
+    free (city_query);
 }
